0e88ce84f306f75c02068c79529c50b4e6ee7dbe
win 11 <win 11@DESKTOP-T4MQ4KH>
2022-04-07 10:52:41 +0530

    Index page changes

--- MasterGenerator.Data/Migrations/20220405104847_DealDetails_ProjectId_migration.Designer.cs
+++ MasterGenerator.Data/Migrations/20220405104847_DealDetails_ProjectId_migration.Designer.cs
@@ -4,6 +4,7 @@ using MasterGenerator.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MasterGenerator.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220405104847_DealDetails_ProjectId_migration")]
+    partial class DealDetails_ProjectId_migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
